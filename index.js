3830b4721ea78da0842db5e527931c9835dd30dd
femas1 <federico.massara2@gmail.com>
2022-10-02 11:46:40 +0200

    added js for toggling active rating

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ratingNumbers = document.querySelectorAll('.container__rating__number');
+
+const removeActiveClasses = function() {
+    ratingNumbers.forEach(number => number.classList.remove('active'));
+}
+
+ratingNumbers.forEach(number => {
+    
+
+    number.addEventListener('click', ()=> {
+        removeActiveClasses();
+        number.classList.toggle('active');
+    })
+})
