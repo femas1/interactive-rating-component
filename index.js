b87878dd019d3af5dea0a48ecb4e3db8bb4a334d
femas1 <federico.massara2@gmail.com>
2022-10-03 10:06:07 +0200

    added js functionality show thank you page

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const ratingNumbers = document.querySelectorAll('.container__rating__number');
+const submitBtn = document.querySelector('.container__button');
+const container = document.querySelector('.container');
+const containerThx = document.querySelector('.container__thanks')
 
 const removeActiveClasses = function() {
     ratingNumbers.forEach(number => number.classList.remove('active'));
@@ -11,4 +14,9 @@ ratingNumbers.forEach(number => {
         removeActiveClasses();2
         number.classList.toggle('active');
     })
+})
+
+submitBtn.addEventListener('click', () => {
+    container.classList.add('hidden');
+    containerThx.classList.remove('hidden');
 })
